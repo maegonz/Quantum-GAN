b03310e24c28928239f256f3450521b506a3e243
maegonz <manutonymanu@gmail.com>
2026-01-18 15:53:31 +0100

    Add argument to custum biais or not

--- GANs/generator.py
+++ GANs/generator.py
@@ -3,7 +3,7 @@ from .blocks import GenBlock
 
 ## Generator network for a Deep Convolutional GAN (DCGAN)
 class Generator(nn.Module):
-    def __init__(self, latent_dim, img_channels, num_features=64):
+    def __init__(self, latent_dim, img_channels, num_features=64, biais=True):
         """
         Initialize the Generator network.
         
@@ -16,19 +16,22 @@ class Generator(nn.Module):
         num_features : int, Optional
             Number of features in the first convolutional layer.
             Defaults to 64.
+        biais : bool, Optional
+            Whether to use random biais or custom biais in the convolution layers.
+            Defaults to True.
         """
         super(Generator, self).__init__()
 
         self.generator = nn.Sequential(
             # From latent vector Z to feature map
-            GenBlock(latent_dim, num_features * 8, stride=1, biais=True),
+            GenBlock(latent_dim, num_features * 8, stride=1, biais=biais),
             # Upsampling layers to increase spatial dimension
             # (num_features*8) x 4 x 4
-            GenBlock(num_features * 8, num_features * 4, biais=True),
+            GenBlock(num_features * 8, num_features * 4, biais=biais),
             # (num_features*4) x 8 x 8
-            GenBlock(num_features * 4, num_features * 2, biais=True),
+            GenBlock(num_features * 4, num_features * 2, biais=biais),
             # (num_features*2) x 16 x 16
-            GenBlock(num_features * 2, num_features, biais=True),
+            GenBlock(num_features * 2, num_features, biais=biais),
             # (num_features) x 32 x 32
             nn.ConvTranspose2d(num_features, img_channels),
             nn.Tanh()  # Output activation to map values between -1 and 1
