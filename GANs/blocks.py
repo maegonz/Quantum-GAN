636391fe1ae02169d107da7a6f5ef7b15281ac3c
maegonz <manutonymanu@gmail.com>
2026-01-18 15:41:34 +0100

    Add kernel_size as an argument

--- GANs/blocks.py
+++ GANs/blocks.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 ## Convolutional Block for GAN's Generator network
 class GenBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=2, padding=1, out_padding=0, biais=False, **kwargs):
+    def __init__(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, out_padding=0, biais=False, **kwargs):
         """
         Initialize a convolutional transpose block for a generator network.
         
@@ -19,7 +19,7 @@ class GenBlock(nn.Module):
             Additional keyword arguments to pass to the convolution layer
         """
         super(GenBlock, self).__init__()
-        self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=stride, padding=padding, output_padding=out_padding, bias=biais, **kwargs)
+        self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, output_padding=out_padding, bias=biais, **kwargs)
         self.norm = nn.BatchNorm2d(out_channels)
         # self.norm = nn.InstanceNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
@@ -33,7 +33,7 @@ class GenBlock(nn.Module):
     
 ## Convolutional block used in the Discriminator network
 class DiscBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=2, padding=1, **kwargs):
+    def __init__(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, **kwargs):
         """
         Initialize a convolutional block for the Discriminator.
         
@@ -51,7 +51,7 @@ class DiscBlock(nn.Module):
             Additional keyword arguments to pass to the convolution layer
         """
         super(DiscBlock, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=stride, padding=padding, bias=True, **kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=True, **kwargs)
         # Normalize features for better training stability
         self.norm = nn.BatchNorm2d(out_channels)
         # self.norm = nn.InstanceNorm2d(out_channels)
