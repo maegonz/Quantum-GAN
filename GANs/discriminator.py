46628371eb05788f36b36a6a5bdfa7e44f1cbcd3
maegonz <manutonymanu@gmail.com>
2026-01-18 16:02:32 +0100

    DCGan discriminator

--- GANs/discriminator.py
+++ GANs/discriminator.py
@@ -1,42 +1,41 @@
 import torch.nn as nn
 from .blocks import DiscBlock
 
-## PatchGAN Discriminator network for real vs fake image patches classification
 class Discriminator(nn.Module):
-    def __init__(self, in_channels, num_features=64):
-        """
+    def __init__(self, in_channels=3, num_features=64, biais=True):
+        """ 
         Initialize the Discriminator network.
-        
-        Parameters
-        ----------
-        in_channels : int
-            Number of input channels
-        num_features : int, optional
-            Number of features in the first convolutional layer. Default is 64.
+
+        Params
+        -------
+        in_channels : int, Optional
+            Number of input channels.
+            Defaults to 3.
+        num_features : int, Optional
+            Base number of features for the convolutional layers.
+            Defaults to 64.
+        biais : bool, Optional
+            Whether to use bias in the convolution layers.
+            Defaults to True.
         """
         super(Discriminator, self).__init__()
-        # Initial convolution block without normalization (as per PatchGAN architecture)
-        self.initial = nn.Sequential(
-            nn.Conv2d(in_channels, num_features, kernel_size=4, stride=2, padding=1, padding_mode="reflect"),
-            nn.LeakyReLU(0.2, inplace=True)
+
+        self.discriminator = nn.Sequential(
+            # Input: in_channels x 64 x 64
+            nn.Conv2d(in_channels, num_features, kernel_size=4, stride=2, padding=1, bias=biais),
+            nn.LeakyReLU(0.2, inplace=True),
+            # num_features x 32 x 32
+            DiscBlock(num_features, num_features * 2, biais=biais),
+            # (num_features*2) x 16 x 16
+            DiscBlock(num_features * 2, num_features * 4, biais=biais),
+            # (num_features*4) x 8 x 8
+            DiscBlock(num_features * 4, num_features * 8, biais=biais),
+            # (num_features*8) x 4 x 4
+            nn.Conv2d(num_features * 8, 1, kernel_size=4, stride=1, padding=0, bias=biais),
+            # Output: 1 x 1 x 1
+            nn.Sigmoid()
         )
-
-        # Downsampling layers to reduce spatial dimensions and increase feature depth
-        self.downsampling = nn.Sequential(
-            DiscBlock(num_features, num_features * 2, stride=2),
-            DiscBlock(num_features * 2, num_features * 4, stride=2),
-            DiscBlock(num_features * 4, num_features * 8, stride=1)
-        )
-
-        # Final convolution to produce a single-channel output (real/fake prediction)
-        self.final = nn.Conv2d(num_features * 8, 1, kernel_size=4, stride=1, padding=1)
-
-        # Output activation to map values between 0 and 1
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        output = self.initial(x)
-        output = self.downsampling(output)
-        output = self.final(output)
-        output = self.sigmoid(output)
-        return output
+    
+    def forward(self, input):
+        output = self.discriminator(input)
+        return output.view(-1, 1).squeeze(1)
